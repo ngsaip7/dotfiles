635594cbcdf7a24737eb32833b948ae3a65a45fd
NG <ngsaip7@gmail.com>
2018-09-27 23:09:02 +0530

    setup: added .vimrc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 # If already installed it won't do anything
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# remove .zshrc from root directory
+# rename .zshrc from root directory
 # symblink .zshrc in dotfiles directory to root folder
-rm ~/.zshrc
+mv ~/.zshrc ~/.old_zshrc
 sh -c "$(ln -sv ~/workspace/dotfiles/.zshrc ~)"
+
+# rename .vimrc from root directory
+# symlink .vimrc in dotfiles to root folder
+sh -c "$(ln -sv ~/workspace/dotfiles/.vimrc ~)"
