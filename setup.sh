3f1ec7ac591ab1e0c87564235e3b10be7ecceb57
NG Sai Prasanth <ng@NGs-MacBook-Pro.local>
2018-09-27 21:22:12 +0530

    setup: remove zshrc & symlink to zshrc from the dotfiles dir

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 # download and install oh-my-zsh
 # If already installed it won't do anything
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# remove .zshrc from root directory
+# symblink .zshrc in dotfiles directory to root folder
+rm ~/.zshrc
+sh -c "$(ln -sv ~/workspace/dotfiles/.zshrc ~)"
