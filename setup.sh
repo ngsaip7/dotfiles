95de5883b68d8eb52164a100cc944c7951ac1522
NG <ngsaip7@gmail.com>
2018-09-30 07:03:11 +0530

    Paths: Added rvm paht

--- setup.sh
+++ setup.sh
@@ -2,19 +2,7 @@
 # If already installed it won't do anything
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# rename .zshrc from root directory
-# symblink .zshrc in dotfiles directory to root folder
-# mv ~/.zshrc ~/.old_zshrc
-sh -c "$(ln -sv ~/workspace/dotfiles/.zshrc ~)"
-
-# rename .vimrc from root directory
-# symlink .vimrc in dotfiles to root folder
-sh -c "$(ln -sv ~/workspace/dotfiles/.vimrc ~)"
-
-# symlink .zsh_profile in dotfiles to root folder
-sh -c "$(ln -sv ~/workspace/dotfiles/.zsh_profile ~)"
-
-# symlink .aliases in dotfiles to root folder
-sh -c "$(ln -sv ~/workspace/dotfiles/.aliases ~)"
+# installing rvm & stable ruby version
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 source ~/.zshrc
